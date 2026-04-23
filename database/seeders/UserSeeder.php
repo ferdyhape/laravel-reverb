af5c755348d7859f9dfc90cd1802e4972db00107
Ferdy Hahan Pradana <ferdy.pradana@gameloft.com>
2026-04-23 10:54:55 +0700

    add: user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        for ($i = 0; $i < 20; $i++) {
+            User::factory()->create([
+                'email' => "user$i@gmail.com",
+            ]);
+        }
+    }
+}
